df9bf72e51c1788d9dff141cde7f310a2e9716b4
Victor Rivas <vrivas@ujaen.es>
2020-11-20 23:26:24 +0100

    Nueva versión con preferencias para cada actividad con *getPreferences* Reemplazamos todas las cadenas por recursos String con formateo de contenido (=*placeholders*)

--- app/src/main/java/es/vrivas/ejemplosapartado12preferencias/MainActivity.kt
+++ app/src/main/java/es/vrivas/ejemplosapartado12preferencias/MainActivity.kt
@@ -2,34 +2,36 @@ package es.vrivas.ejemplosapartado12preferencias
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_mostrar_preferencias_guardadas.*
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-
+    // Creación de la actividad
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Establecew evento click para el botón
         bt_iniciar_sesion.setOnClickListener(this)
 
-        // getDefaultSharedPreferences está obsoleto
+        // getDefaultSharedPreferences está obsoleto. Usamos packageName como "alternativa"
         PREFERENCES_FILENAME=getPackageName() + "_preferences"
     }
 
+    // Recuperamos las preferencias que haya almacenadas
     override fun onPostResume() {
         super.onPostResume()
-        recuperar_preferencias()
+        recuperar_preferencias_compartidas()
+        recuperar_preferencias_activity()
     }
-    fun recuperar_preferencias() {
-        Log.i( "VICTOR", PREFERENCES_FILENAME)
+
+    // Métodos para preferencias compartidas
+    fun recuperar_preferencias_compartidas() {
         val preferencias = getSharedPreferences(PREFERENCES_FILENAME, MODE_PRIVATE)
         var login: String?=""
         var password: String?=""
@@ -45,7 +47,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         te_login.setText(login!!.toString())
         te_password.setText(password!!.toString())
     }
-    fun guardar_preferencias() {
+    fun guardar_preferencias_compartidas() {
         val preferencias = getSharedPreferences(PREFERENCES_FILENAME, MODE_PRIVATE)
         val editorPreferencias = preferencias.edit()
         // La preferencia "RECORDAR" siempre se almacena
@@ -57,29 +59,58 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
         editorPreferencias.apply()
     }
+
+
+    // Métodos para preferencias_activity
+    fun recuperar_preferencias_activity() {
+        val preferencias = getPreferences(MODE_PRIVATE)
+        val palabras_recuperadas=preferencias.getStringSet(TOKEN_PALABRAS, setOf<String?>()) // Añadir o modificar
+        tv_palabras_main.setText( palabras_recuperadas.toString())
+    }
+
+    fun guardar_preferencias_activity() {
+        val preferencias = getPreferences(MODE_PRIVATE)
+        val editorPreferencias = preferencias.edit()
+        editorPreferencias.putStringSet(TOKEN_PALABRAS, setOf<String?>(
+                "Yo",
+                "soy",
+                "la",
+                "actividad",
+                "principal")
+        ) // Añadir o modificar
+        editorPreferencias.apply()
+    }
+
+
+    // Evento click del botón.
     override fun onClick(v: View?) {
-        guardar_preferencias()
+        guardar_preferencias_compartidas()
+        guardar_preferencias_activity()
         try {
             val intent= Intent(this, MostrarPreferenciasGuardadas::class.java)
             startActivity(intent)
         } catch (e: Exception) {
             Toast.makeText(
-                this,
-                "No se pudo abrir actividad: ${e.localizedMessage}",
-                Toast.LENGTH_LONG
+                    this,
+                    "No se pudo abrir actividad: ${e.localizedMessage}",
+                    Toast.LENGTH_LONG
             ).show()
         }
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
-        recuperar_preferencias()
+        recuperar_preferencias_compartidas()
     }
 
     companion object {
+        // Tokens para preferencias compartidas
         const val TOKEN_LOGIN = "token login"
         const val TOKEN_RECORDAR = "token recordar"
         const val TOKEN_PASSWORD = "token password"
         var PREFERENCES_FILENAME = ""
+
+        // Tokens para preferencias actividad
+        const val TOKEN_PALABRAS = "token para listado de palabras"
     }
 }
--- app/src/main/java/es/vrivas/ejemplosapartado12preferencias/MostrarPreferenciasGuardadas.kt
+++ app/src/main/java/es/vrivas/ejemplosapartado12preferencias/MostrarPreferenciasGuardadas.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_mostrar_preferencias_guardadas.*
 
 class MostrarPreferenciasGuardadas : AppCompatActivity() {
@@ -16,14 +17,41 @@ class MostrarPreferenciasGuardadas : AppCompatActivity() {
         val password = preferencias.getString(MainActivity.TOKEN_PASSWORD, "VALOR_POR_DEFECTO")
         val recordar = preferencias.getBoolean(MainActivity.TOKEN_RECORDAR, false)
 
-        tv_login.text= "Login: "+login!!.toString()
-        tv_password.text="Password: "+password!!.toString()
-        tv_recordar.text="Recordar: "+(if( recordar ) "Sí" else "No")
+        tv_login.text= String.format( getString(R.string.login_mostrar),
+                login!!.toString())
+        tv_password.text=String.format( getString(R.string.password_mostrar),
+                password!!.toString())
+        tv_recordar.text=String.format( getString(R.string.recordar_mostrar),
+                if( recordar ) "Sí" else "No")
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onPostCreate(savedInstanceState, persistentState)
-
-
+    override fun onResume() {
+        super.onResume()
+        recuperar_preferencias_activity()
     }
+
+    override fun onPause() {
+        guardar_preferencias_activity()
+        super.onPause()
+    }
+    // Métodos para preferencias_activity
+    fun recuperar_preferencias_activity() {
+        val preferencias = getPreferences(MODE_PRIVATE)
+        val palabras_recuperadas=preferencias.getStringSet(MainActivity.TOKEN_PALABRAS, setOf<String?>()) // Añadir o modificar
+        tv_palabras_mostrar.setText( palabras_recuperadas.toString())
+    }
+
+    fun guardar_preferencias_activity() {
+        val preferencias = getPreferences(MODE_PRIVATE)
+        val editorPreferencias = preferencias.edit()
+        editorPreferencias.putStringSet(MainActivity.TOKEN_PALABRAS,
+                setOf<String?>(
+                        "Soy",
+                        "la",
+                        "actividad",
+                        "MostrarPreferenciasGuardadas")
+        ) // Añadir o modificar
+        editorPreferencias.apply()
+    }
+
 }
